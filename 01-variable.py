28b4b0b3be5f8c979a68f33a17fd13c36e2e0115
Ramerlinj <ramerlint2008@gmail.com>
2026-01-14 23:42:49 -0400

    Add greeting script with personal introduction

---
+++ 01-variable.py
@@ -0,0 +1,17 @@
+# Hacer saludo
+
+nombre = "Ramerlin"
+edad = 17
+asiste_escuela = True
+sabe_programar = False
+calificacion_promedio = 9.5
+
+
+presentacion = (
+    "Hola, " + nombre + "!",
+    "Tienes " + str(edad) + " años.",
+    "¿Asistes a la escuela? " + str(asiste_escuela) + ".",
+    "¿Sabes programar? " + str(sabe_programar) + ".",
+    "Tu calificación promedio es " + str(calificacion_promedio) + ".",
+)
+print(presentacion)
