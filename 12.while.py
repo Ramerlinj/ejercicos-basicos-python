1dbd69ce80099b21bf5d3ee921c4dcc1ab04239c
Ramerlinj <ramerlint2008@gmail.com>
2026-01-23 19:54:29 -0400

    Add while loop examples for counting and iterating through a list of programming languages

---
+++ 12.while.py
@@ -0,0 +1,14 @@
+lengujaes = ["Python", "Java", "C++", "JavaScript", "Ruby"]
+
+count = 0
+    
+while count <= 5:
+    print("El valor de count es:", count) 
+    count += 1
+    
+    
+i=0
+
+while i < len(lengujaes):
+    print("Lenguaje en la posicion", i, "es:", lengujaes[i])
+    i += 1
