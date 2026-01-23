206e481f712f0d38134f99b27b81b1915d41b329
Ramerlinj <ramerlint2008@gmail.com>
2026-01-23 19:54:16 -0400

    Add initial implementation of language list with various print statements

---
+++ 10.listas.py
@@ -0,0 +1,11 @@
+lengujaes = ["Python", "Java", "C++", "JavaScript", "Ruby"]
+lengujaes[1] = "Java SE"
+print(lengujaes)
+print(lengujaes[1])
+print(lengujaes[-1])
+print(lengujaes[1:4])
+print(lengujaes[1:])
+print(lengujaes[:3])
+
+
+
