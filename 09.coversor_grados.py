254b4ac9f2f8539bab14e293b6cebaf5357da163
Ramerlinj <ramerlint2008@gmail.com>
2026-01-23 19:54:08 -0400

    Add Celsius to Fahrenheit converter in coversor_grados.py

---
+++ 09.coversor_grados.py
@@ -0,0 +1,4 @@
+# coversor de celcis a a grados fahrenheit
+celsius = float(input("Ingrese la temperatura en grados Celsius: "))
+fahrenheit = (celsius * 9/5) + 32 #formula de conversion
+print(f"La temperatura en grados Fahrenheit es: {fahrenheit} °F")
