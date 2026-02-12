30f3b418932012211b0c09140b02c5fae49a2834
Ramerlinj <ramerlint2008@gmail.com>
2026-02-12 09:50:38 -0400

    practica

---
+++ poo/Practica_clase.py
@@ -0,0 +1,43 @@
+class Dispositivo:
+    def __init__(self, marca):
+        self.marca = marca
+        self.__encedido = False
+        
+    def encender(self):
+        self.__encedido = True
+        return f"El dispositivo de {self.marca} esta encendido..."
+    
+class Telefono(Dispositivo):
+    def __init__(self, marca,numero):
+        super().__init__(marca)
+        self.numero = numero
+        
+    def hacer_llamada(self, contacto):
+        return f"llamada a {contacto} desde el {self.numero}...."
+
+class Camara:
+    def __init__(self, megapixeles):
+        self.megapixeles = megapixeles
+        
+    def tomar_foto(self):
+        return f"Wisssskiiii. se tomo foto desde a {self.megapixeles}"
+    
+class Smartphone(Telefono, Camara):
+    def __init__(self, marca, numero, megapixeles, modelo):
+        Telefono.__init__(self,marca, numero)
+        Camara.__init__(self,megapixeles)
+        self.modelo = modelo
+    def encender(self):
+        estado_base = super().encender()
+        return f"{estado_base} Cargando mi Androide... Bienvenido al {self.modelo}"
+
+#OJETO
+mi_celular = Smartphone("Tecno Sport", "849-433-0448", 500, "Pro MAXIMA")
+print(mi_celular.encender())
+print(mi_celular.hacer_llamada('Juan'))
+print(mi_celular.tomar_foto())
+        
+    
+    
+        
+        
